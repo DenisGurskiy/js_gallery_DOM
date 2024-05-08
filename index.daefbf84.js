638b643d75dff1928262de579e46bee6ca7db55f
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-05-08 11:10:39 +0200

    make build

---
+++ index.daefbf84.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("ul"),t=document.querySelector("#largeImg");e.addEventListener("click",function(e){e.preventDefault(),t.src=e.target.closest("a").href});
+//# sourceMappingURL=index.daefbf84.js.map
